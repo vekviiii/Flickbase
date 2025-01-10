23cc08bceb9ee971d5e2d1fda8cdd5fc02c83473
vekviiii <vivekkumar124356@gmail.com>
2025-01-11 01:31:37 +0530

    final push for the night

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
+import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     proxy: {
-      '/api': {  // use /api without quotes
+      '/api': {  // proxying /api requests
         target: process.env.URL_CLIENT,
         changeOrigin: true,
         secure: false,
       }
     }
   },
-  optimizeDeps:{
-    esbuildOptions:{
-      plugins:[esbuildCommonjs(['react-moment'])]
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [esbuildCommonjs(['react-moment'])]
     }
-  }
-})
+  },
+  build: {
+    sourcemap: true, // Enable source maps for production builds
+  },
+});
