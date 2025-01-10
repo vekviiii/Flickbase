335faccd8af656df3ebdb89aa1e2f456478f4f0a
vekviiii <vivekkumar124356@gmail.com>
2025-01-11 01:00:55 +0530

    Added connection

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server:{
     proxy: {
       '/api': {  // use /api without quotes
-        target: "http://127.0.0.1:3001",
+        target: process.env.URL_CLIENT,
         changeOrigin: true,
         secure: false,
       }
